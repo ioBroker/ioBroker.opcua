6754e14360f533a8fc47c349c3e397733eadd39f
GermanBluefox <dogafox@gmail.com>
2017-10-06 18:24:55 +0200

    (bluefox) add words.js

---
+++ admin/words.js
@@ -0,0 +1,43 @@
+systemDictionary = {
+    "OPC UA adapter settings": {"en": "OPC UA adapter settings",  "de": "OPC UA Adapter-Einstellungen",  "ru": "Настройки драйвера OPC UA"},
+    "Type:":                  {"en": "Type:",                  "de": "Typ:",                   "ru": "Тип:"},
+    "Port:":                  {"en": "Port:",                  "de": "Port:",                  "ru": "Порт:"},
+    "User:":                  {"en": "User:",                  "de": "Username:",              "ru": "Имя пользователя:"},
+    "Password:":              {"en": "Password:",              "de": "Kennwort:",              "ru": "Пароль:"},
+    "Password confirmation:": {"en": "Password confirmation:", "de": "Kennwort-Wiederholung:", "ru": "Подтверждение пароля:"},
+    "URL:":                   {"en": "URL:",                   "de": "URL:",                   "ru": "URL:"},
+    "Secure:":                {"en": "Secure:",                "de": "SSL:",                   "ru": "SSL:"},
+    "Public certificate:":    {"en": "Public certificate:",    "de": "Publikzertifikat:",      "ru": "'Public' сертификат:"},
+    "Private certificate:":   {"en": "Private certificate:",   "de": "Privatzertifikat:",      "ru": "'Private' сертификат:"},
+    "Patterns:":              {"en": "Subscribe patterns:",    "de": "Subscribe patterns:",    "ru": "Patterns:"},
+    "Divided by comma":       {
+        "en": "Divided by comma, e.g. 'opcua/0/#,javascript/#'",
+        "de": "Mit Komma getrennt, z.B 'opcua/0/#,javascript/#'",
+        "ru": "Использовать запятую, как разделитеть. Например 'opcua/0/#,javascript/#'"
+    },
+    "Mask to publish":       {
+        "en": "e.g. 'opcua.0.*,javascript.*'",
+        "de": "z.B 'opcua.0.*,javascript.*'",
+        "ru": "Использовать запятую, как разделитеть. Например 'opcua.0.*,javascript.*'"
+    },
+    "Store only on change:":  {"en": "Store/Send only on change:",  "de": "Speichern/Senden nur bei Änderung:",  "ru": "Сохранять/Отсылать только изменения:"},
+    "Test connection":        {"en": "Test connection with Server", "de": "Teste Verbindung zum Server", "ru": "Проверить настройки"},
+    "Result: ":               {"en": "Result: ",               "de": "Ergebnis: ",             "ru": "Результат: "},
+    "connected":              {"en": "connected",              "de": "verbunden",              "ru": "успешно"},
+    "Main settings":          {"en": "Main settings",          "de": "Allgemeine Einstellungen","ru": "Основные настройки"},
+    "Connection settings":    {"en": "Connection settings",    "de": "Verbindungseinstellungen",    "ru": "Настройки соединения"},
+    "Authentication settings":{"en": "Authentication settings","de": "Authentication Einstellungen", "ru": "Настройки аутентификации"},
+    "Adapter settings":       {"en": "OPC UA settings",        "de": "OPC UA Einstellungen",     "ru": "Настройки OPC UA"},
+    "Send states (ack=true) too:":    {
+        "en": "Send states (ack=true) too:",
+        "de": "Sende auch Zustände (ack=true):",
+        "ru": "Посылать не только команды, но и состояния (ack=true):"
+    },
+    "Set certificates or load it first in the system settings (right top).":    {
+        "en": "Set certificates or load it first in the system settings (right top).",
+        "de": "Setze Zertificate oder lade die erst unter System/Einstellungen (oben rechts).",
+        "ru": "Нужно выбрать сертификаты или сначала загрузить их в системных настройках (вверху справа)."
+    },
+    "Server":                 {"en": "Server",                  "de": "Server",                 "ru": "Сервер"},
+    "Client":                 {"en": "Client",                  "de": "Client",                 "ru": "Клиент"}
+};
